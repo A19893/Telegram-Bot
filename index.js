da2da82efbc19527f2515d4a35e0e5121623711b
yash <yasharora2678@gmail.com>
2024-01-28 22:35:23 +0530

    Done with the mini project

---
+++ index.js
@@ -0,0 +1,143 @@
+require("dotenv").config()
+const { Telegraf } = require('telegraf')
+const { message } = require('telegraf/filters')
+
+const bot = new Telegraf(process.env.BOT_TOKEN)
+const binarySearchCode = `
+// C++ program to implement iterative Binary Search
+#include <bits/stdc++.h>
+using namespace std;
+
+// An iterative binary search function.
+int binarySearch(int arr[], int l, int r, int x) {
+    while (l <= r) {
+        int m = l + (r - l) / 2;
+
+        // Check if x is present at mid
+        if (arr[m] == x)
+            return m;
+
+        // If x greater, ignore left half
+        if (arr[m] < x)
+            l = m + 1;
+
+        // If x is smaller, ignore right half
+        else
+            r = m - 1;
+    }
+
+    // If we reach here, then element was not present
+    return -1;
+}
+
+// Driver code
+int main(void) {
+    int arr[] = { 2, 3, 4, 10, 40 };
+    int x = 10;
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int result = binarySearch(arr, 0, n - 1, x);
+    (result == -1)
+        ? cout << "Element is not present in array"
+        : cout << "Element is present at index " << result;
+    return 0;
+}`;
+const linearSearchCode = `
+// C++ program to implement Linear Search
+#include <iostream>
+using namespace std;
+
+int linearSearch(int arr[], int n, int x) {
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == x)
+            return i;
+    }
+    return -1;
+}
+
+int main() {
+    int arr[] = { 2, 3, 4, 10, 40 };
+    int x = 10;
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int result = linearSearch(arr, n, x);
+    (result == -1)
+        ? cout << "Element is not present in array"
+        : cout << "Element is present at index " << result;
+    return 0;
+}`;
+
+const mergeSortCode = `
+// C++ program to implement Merge Sort
+#include <iostream>
+using namespace std;
+
+void merge(int arr[], int l, int m, int r) {
+    int n1 = m - l + 1;
+    int n2 = r - m;
+
+    int L[n1], R[n2];
+
+    for (int i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (int j = 0; j < n2; j++)
+        R[j] = arr[m + 1 + j];
+
+    int i = 0;
+    int j = 0;
+    int k = l;
+
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        } else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+void mergeSort(int arr[], int l, int r) {
+    if (l >= r)
+        return;
+    int m = l + (r - l) / 2;
+    mergeSort(arr, l, m);
+    mergeSort(arr, m + 1, r);
+    merge(arr, l, m, r);
+}
+
+int main() {
+    int arr[] = { 12, 11, 13, 5, 6, 7 };
+    int arr_size = sizeof(arr) / sizeof(arr[0]);
+
+    mergeSort(arr, 0, arr_size - 1);
+
+    cout << "Sorted array: \\n";
+    for (int i = 0; i < arr_size; i++)
+        cout << arr[i] << " ";
+    cout << "\\n";
+
+    return 0;
+}`;
+bot.start((ctx) => ctx.reply('Welcome/to Telegram Bot By Yash \n We offer Some Services For Our Basic Coding Algos in C++ Language \n binarySearch \n mergeSort \n linearSearch'))
+bot.command('binarySearch', (ctx) => ctx.reply(binarySearchCode))
+bot.command('linearSearch', (ctx) => ctx.reply(linearSearchCode))
+bot.command('mergeSort', (ctx) => ctx.reply(mergeSortCode))
+bot.on('sticker', (ctx) => ctx.reply('👍'))
+bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+bot.launch()
+
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
